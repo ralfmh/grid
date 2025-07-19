e20dc7f3ad10cf8cc2cbaa1b484cb99cb91ec6b4
Jonathan Schuster <jps@jonathan859.com>
2025-07-19 14:32:09 +0200

    Adds script and config file for Astyle, correctly indents Grid.nvgt

---
+++ FormatAstyle.bat
@@ -0,0 +1,7 @@
+:: This assumes you have astyle.exe (https://sourceforge.net/projects/astyle/) in your path or this folder.
+@echo off
+setlocal
+echo Running AStyle on %TARGET_FOLDER%...
+astyle --options=.astylerc --suffix=none --recursive "src/*.nvgt"
+echo Formatting complete!
+pause
